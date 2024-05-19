7d90e18a94155b3f91bb42df2f9d3d698fb8bd61
kevin <if23b277@technikum-wien.at>
2024-05-19 16:24:32 +0200

    made first Test

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,9 +1,9 @@
-describe('String Calculator', function() {
+describe('Dice Calculator', function() {
 	beforeEach(function() {
-		calculator = new StringCalculator();
+		calculator = new DiceCalculator();
 	});
 
-	it('should return 0 for an empty string', function() {
-		expect(calculator.add('')).toEqual(0);
+	it('should return 6 for [ 1, 2, 3, 4, 5 ] ', function() {
+		expect(calculator.add('[ 1, 2, 3, 4, 5 ]')).toEqual(6);
 	});
 });
