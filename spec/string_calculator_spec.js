32c1dae65358205bf210a77f6fa798e12ef21b06
Elaine <elaine.fink19@gmail.com>
2024-05-19 16:31:40 +0200

    Second failing task - Elaine

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -6,4 +6,8 @@ describe('Dice Calculator', function() {
 	it('should return 6 for [ 1, 2, 3, 4, 5 ] ', function() {
 		expect(calculator.add('[ 1, 2, 3, 4, 5 ]')).toEqual(6);
 	});
+	it('should return 4 for [ 2, 2, 3, 3 ] ', function() {
+		expect(calculator.add('[ 2, 2, 3, 3 ]')).toEqual(4);
+	});
+
 });
