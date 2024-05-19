7fb3ea06fe2a17fb7a4dc9f6c4a3cf213e1e48c3
kevin <if23b277@technikum-wien.at>
2024-05-19 17:00:38 +0200

    Kevin: test case 2

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,23 @@
 // StringCalculator.js
 function DiceCalculator() {}
 DiceCalculator.prototype.add = function(string_numbers) {
-	return 6;
-};
+
+
+        var number  = string_numbers.split(",");
+        var even = 0;
+		var odd = 0;
+		for (let i = 0; i < number.length; i++) {
+			
+			if(number[i] %2 ==0){
+				
+				even += parseInt(number[i]);
+			}
+			else{
+	
+				odd += parseInt(number[i]);
+			}
+		}
+        return (odd - even) * 2;
+
+    }
+;
